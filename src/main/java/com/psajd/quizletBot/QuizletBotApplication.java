1f496c650ddb5bae296905538328ec9adf7d32a0
Gomzyakov I. A <76881688+psajd@users.noreply.github.com>
2023-03-30 00:30:26 +0300

    initial spring boot commit

---
+++ src/main/java/com/psajd/quizletBot/QuizletBotApplication.java
@@ -0,0 +1,13 @@
+package com.psajd.quizletBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizletBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizletBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psajd/quizletBot/QuizletBotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.psajd.quizletBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizletBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void dbTest() {
+
+    }
+}
